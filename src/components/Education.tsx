541dbfb5ad64b0857c8e029b76775939bc84345d
Fajar Abdurrohman <48176273+fajar2510@users.noreply.github.com>
2023-06-06 08:15:04 +0700

    patch1

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -36,7 +36,7 @@ const Education = () => {
                     <p className="text-base font-normal text-slate-600 dark:text-slate-400">Build any project and assignment about Web and Android Applications and
                         Join game development community and build some games.
                     </p>
-                    <Link href="/pdf/17051214047SPK.pdf" donwlod target='_blank' className="mt-4 inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
+                    <Link href="/pdf/17051214047SPK.pdf" download target='_blank' className="mt-4 inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
                         SPK or <span className=' p-1 italic'> Graduation Confirmation Letter</span> <svg className="w-3 h-3 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></Link>
 
                 </li>
