8aefaf7bdb447f0cf56b3d0b22365561e93afcfa
Fajar Abdurrohman <48176273+fajar2510@users.noreply.github.com>
2024-12-26 04:44:37 +0000

    google-analytic

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,13 +3,24 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html className="bg-white dark:bg-slate-800 scroll-smooth" lang="en">
-      <Head />
-      
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-E72GCSKST1"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-E72GCSKST1');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
-
     </Html>
   )
 }
