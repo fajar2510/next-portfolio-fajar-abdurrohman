84b0acc674fade07e83b2fe7981dca2220bc717e
Fajar Abdurrohman <sepertiseharusnyafajar@gmail.com>
2025-01-13 17:27:31 +0700

    refactor: format code for improved readability in AnimationDesign component

--- src/components/AnimationDesign.tsx
+++ src/components/AnimationDesign.tsx
@@ -6,7 +6,10 @@ interface VideoProps {
 }
 
 const videos: VideoProps[] = [
-  { videoId: "7hqffNLydc8", title: "Kominfo 2D Animation Bootcamp - Fajar Abdurrohman" },
+  {
+    videoId: "7hqffNLydc8",
+    title: "Kominfo 2D Animation Bootcamp - Fajar Abdurrohman",
+  },
   { videoId: "AWrRxuIc6kA", title: "Ninu Kucing Pemalas - Fajar Abdurrohman" },
 ];
 
@@ -23,9 +26,13 @@ const AnimationDesign = () => {
       <h1 className="lg:text-3xl md:text-2xl text-lg text-center font-bold mb-2 lg:mb-5 text-gray-900 dark:text-white">
         2D Animation Project
       </h1>
-      <div className="h-[50rem] lg:h-[30rem] shadow-brutalism w-auto rounded-3xl p-2 lg:p-10 flex flex-col lg:flex-row gap-16 items-center justify-center bg-[url('https://ik.imagekit.io/fajarblog/porto//gradient-bg.jpg')] bg-cover"></div>
+      <div className="h-[50rem] lg:h-[30rem] shadow-brutalism w-auto rounded-3xl p-2 lg:p-10 flex flex-col lg:flex-row gap-16 items-center justify-center bg-[url('https://ik.imagekit.io/fajarblog/porto//gradient-bg.jpg')] bg-cover">
         {videos.map((video) => (
-          <YouTubeEmbed key={video.videoId} videoId={video.videoId} title={video.title} />
+          <YouTubeEmbed
+            key={video.videoId}
+            videoId={video.videoId}
+            title={video.title}
+          />
         ))}
       </div>
     </section>
